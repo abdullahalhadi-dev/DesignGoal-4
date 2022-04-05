55f5150f52979fe8d2f4c8a2253c42a7520a6157
Abdullah Al Hadi <abdullahalhadi317@gmail.com>
2022-04-05 10:24:04 +0600

    work update

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const body = document.querySelector('body'),
     nav = document.querySelector('nav'),
     modeToggle = document.querySelector('.dark-light'),
-    searchToggle = document.querySelector('.searchBox');
+    searchToggle = document.querySelector('.searchToggle'),
+    siderbarClose = document.querySelector('.siderbarClose');
 
 
 
     modeToggle.addEventListener("click", () => {
         modeToggle.classList.toggle("active");
         body.classList.toggle("dark");
+    })
+
+    searchToggle.addEventListener("click", () => {
+        searchToggle.classList.toggle("active");
+ 
     })
